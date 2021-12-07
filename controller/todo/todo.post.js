f8140900c49c0634f184dd097a6506a0bfa2b882
VladimirChinaev <vladimirchinaev21@gmail.com>
2021-12-07 10:13:29 +0300

    fix bugs

--- controller/todo/todo.post.js
+++ controller/todo/todo.post.js
@@ -1,16 +1,29 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 let tasks = require(path.resolve("db.json"));
 const { v4 } = require("uuid");
 const createTask = (req, res) => {
-    console.log(12351253123);
     const name = req.body.name;
-    if (!name) { return res.status(400).json(" Поле name пустое") };
-    if (name.length < 2) return res.status(400).json("Поле записи меньше 2 х символов");
+    if (!name) {
+        return res.status(400).json(" Поле name пустое");
+    }
+
+    if (name.length < 2)
+        return res.status(400).json("Поле записи меньше 2 х символов");
+
     const repeatTask = tasks.todos.findIndex((el) => el.name === name);
-    if (repeatTask !== -1) return res.status(400).json("Такая задача уже есть!");
+
+    if (repeatTask !== -1) {
+        return res.status(400).json("Такая задача уже есть!");
+    }
+
     try {
-        const task = { uuid: v4(), name: req.body.name, done: "undone", updatedAtt: new Date() };
+        const task = {
+            uuid: v4(),
+            name: req.body.name,
+            done: "undone",
+            updatedAtt: new Date(),
+        };
         tasks.todos = [...tasks.todos, task];
         fs.writeFile("db.json", JSON.stringify(tasks), (err) => {
             if (err) {
@@ -22,5 +35,5 @@ const createTask = (req, res) => {
         console.log(err);
         res.send("askdgaksjd");
     }
-}
+};
 module.exports = createTask;
