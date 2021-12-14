71822281e3581004aab32bd93cffb9aa4380d0bd
VladimirChinaev <vladimirchinaev21@gmail.com>
2021-12-14 11:42:17 +0300

    add registration for user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,51 @@
 const { Router } = require("express");
+const { User } = require("../models");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const e = require("express");
 const router = Router();
 
 // /api/auth/register
-router.post("/register", async (req, res) => {
-    try {
-        const { email, password } = req.body;
+module.export = router.post("/register",
+    [
+        check("email", "worng email").isEmail(),
+        check("password", "Minimum password length 6 characters")
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        try {
+            console.log("__________________________2______________________________");
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Wrong data"
+                })
+            }
+            console.log("__________________________3______________________________");
+            const { email, password } = req.body;
+            console.log("__________________________4______________________________");
+            console.log(email, password);
+            const candidate = await User.findOne({ where: { email } });
+            console.log(candidate);
 
-    } catch (err) {
-        res.status(500).json({ message: "Something was wrong, try again" });
-    }
+            if (candidate) {
+                return res.status(400).json("Such user already exists");
+            }
+            console.log("__________________________5______________________________");
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email: email, password: hashedPassword })
 
-})
+            await user.save();
+            res.status(201).json({ message: "User was created" });
+        } catch (err) {
+            res.status(500).json({ message: "Something was wrong, try again" });
+        }
+
+    })
 
 // /api/auth/login
-router.post("/register", async (req, res) => {
+router.post("/login", async (req, res) => {
 
 })
 
